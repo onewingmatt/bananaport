dba97686551dc2980ac46dba4e6d8c221b7e1207
onewingmatt <onewing@gmail.com>
2026-06-23 18:57:07 -0300

    add Dockerfile, default port 8080

--- server.js
+++ server.js
@@ -203,7 +203,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
